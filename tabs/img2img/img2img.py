0e9a05174ae5e270720762181cb43a4d89569115
arena <arenatemp@mailfence.com>
2023-02-13 19:38:11 +0800

    move to opengl canvas rendering

--- tabs/img2img/img2img.py
+++ tabs/img2img/img2img.py
@@ -1,5 +1,9 @@
-from PyQt5.QtCore import pyqtProperty, pyqtSlot, QObject
-from PyQt5.QtQml import qmlRegisterSingletonType
+from PyQt5.QtCore import pyqtProperty, pyqtSlot, pyqtSignal, QObject, Qt, QRectF, QPointF, QSize
+from PyQt5.QtQml import qmlRegisterSingletonType, qmlRegisterType
+from PyQt5.QtQuick import QQuickFramebufferObject
+from PyQt5.QtGui import QFont, QRadialGradient, QPen, QBrush, QColor, QPainter, QPaintDevice, QOpenGLPaintDevice
+import math
+import copy
 
 class img2img(QObject):
     def __init__(self, parent=None):
@@ -7,3 +11,125 @@ class img2img(QObject):
         self.priority = 1
         self.name = "Img2Img"
         qmlRegisterSingletonType(img2img, "gui", 1, 0, "IMG2IMG", lambda qml, js: self)
+        qmlRegisterType(ImageCanvas, "gui", 1, 0, "ImageCanvas")
+
+class ImageCanvasStates():
+    def __init__(self):
+        self.brushSize = 20
+        self.brushSpacing = 0.1
+        self.brushColor = QColor.fromRgbF(1, 0, 0, 1)
+        self.brushHardness = 0.5
+        self.strokePositions = []
+
+    def synchronize(self):
+        out = copy.copy(self)
+        self.clearStroke()
+        return out
+
+    def addStroke(self, position):
+        self.strokePositions.append(position)
+
+    def clearStroke(self):
+        self.strokePositions = []
+
+    def getBrushAbsoluteSpacing(self):
+        return self.brushSize * self.brushSpacing
+
+    def getBrushColor(self, radius):
+        alpha = 1.0
+        if self.brushHardness != 1:
+            alpha = ((math.cos(radius*math.pi)+1)/2)**(1/(2*self.brushHardness))
+
+        color = QColor(self.brushColor)
+        color.setAlphaF(alpha)
+        return color
+
+class ImageCanvasRenderer(QQuickFramebufferObject.Renderer):
+    def __init__(self, parent=None, size=None):
+        super().__init__()
+        self.size = size
+        self.states = None
+    
+    def createFramebufferObject(self, size):
+        return super().createFramebufferObject(self.size)
+    
+    def synchronize(self, item):
+        self.states = item.states.synchronize()
+
+    def render(self):
+        if self.states and self.states.strokePositions:
+            states = self.states
+
+            device = QOpenGLPaintDevice(self.size)
+            painter = QPainter(device)
+            painter.beginNativePainting()
+
+            while states.strokePositions:
+                p = states.strokePositions.pop(0)
+                p = QPointF(p.x(), self.size.height()-p.y())
+
+                g = QRadialGradient(p.x(), p.y(), states.brushSize/2)
+
+                steps = states.brushSize//4
+                for i in range(0, steps+1):
+                    r = i/steps
+                    g.setColorAt(r, states.getBrushColor(r))
+
+                painter.setPen(Qt.NoPen)
+                painter.setBrush(g)
+
+                painter.drawEllipse(p, states.brushSize, states.brushSize)
+
+            painter.endNativePainting()
+            painter.end()
+            self.states = None
+
+class ImageCanvas(QQuickFramebufferObject):
+    canvasSizeUpdated = pyqtSignal()
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setTextureFollowsItemSize(False)
+
+        self.textureSize = QSize(0,0)
+        self.states = ImageCanvasStates()
+        self.last_pos = None
+
+    def createRenderer(self):
+        return ImageCanvasRenderer(self, self.textureSize)
+
+    @pyqtProperty(QSize, notify=canvasSizeUpdated)
+    def canvasSize(self):
+        return self.textureSize
+    
+    @canvasSize.setter
+    def canvasSize(self, size):
+        self.textureSize = size
+
+    @pyqtSlot(QPointF)
+    def mousePressed(self, position):
+        self.states.addStroke(position)
+        self.last_pos = position
+
+    @pyqtSlot(QPointF)
+    def mouseReleased(self, position):
+        self.states.clearStroke()
+
+    @pyqtSlot(QPointF)
+    def mouseDragged(self, position):
+        if self.last_pos == None:
+            self.states.addStroke(position)
+            self.last_pos = position
+        else:
+            last = QPointF(self.last_pos)
+            s = self.states.getBrushAbsoluteSpacing()
+            v = QPointF(position.x()-last.x(), position.y()-last.y())
+            m = (v.x()*v.x() + v.y()*v.y())**0.5
+            r = m/s
+            if r < 1:
+                return
+            
+            for i in range(1, int(r)+1):
+                f = i*s/m
+                p = QPointF(last.x()+v.x()*f, last.y()+v.y()*f)
+                self.states.addStroke(p)
+            self.last_pos = QPointF(p)
--- tabs/img2img/img2img.qml
+++ tabs/img2img/img2img.qml
@@ -8,115 +8,69 @@ import gui 1.0
 import "../../style"
 import "../../components"
 
-
 Item {
     id: root
+    clip: true
 
-    property var size: 40
-    property var spacing: 0.1
-    property var hardness: 0.5
-
-    function dropoff(x, h) {
-        if(hardness == 1) {
-            return 1.0
-        }
-        return Math.pow((Math.cos(x*Math.PI)+1)/2, 1/(2*h))
+    Media {
+        id: media
+        anchors.fill: parent
+        source: "/run/media/pul/ssd/sd-inference-gui/image.png"
+        sourceWidth: 768
+        sourceHeight: 768
     }
 
-    Canvas {
+    Item {
+        id: overlay
+        x: media.current.x
+        y: media.current.y
+        width: media.current.width
+        height: media.current.height
+    }
+
+    ImageCanvas {
         id: canvas
-        anchors.fill: parent
+        anchors.fill: overlay
         property var mouse_pos: []
         property var do_reset: true
-
-        onPaint: {
-            var ctx;
-
-            ctx = getContext("2d");
-            if(do_reset) {
-                ctx.reset()
-                do_reset = false
-            }
-
-            if(mouse_pos.length == 0) {
-                return;
-            }
-            
-            var p;
-            while(mouse_pos.length != 0) {
-                var p = mouse_pos.shift();
-
-                var g = ctx.createRadialGradient(p.x, p.y, 0, p.x, p.y, root.size/2)
-                for(var x = 0; x <= 1; x += 1/10) {
-                    var s = dropoff(x, root.hardness)
-                    g.addColorStop(x, Qt.rgba(1, 1, 1, s));
-                }
-                ctx.fillStyle = g
-
-                ctx.ellipse(p.x-root.size/2, p.y-root.size/2, root.size, root.size);
-                ctx.fill();
-                ctx.beginPath();
-                ctx.closePath();
-            }
-        }
+        canvasSize: Qt.size(media.sourceWidth, media.sourceHeight)
+        smooth: media.sourceWidth*2 < width && media.sourceHeight*2 < height ? false : true
     }
 
     Timer {
         interval: 17; running: true; repeat: true
-        onTriggered: canvas.requestPaint()
+        onTriggered: canvas.update()
     }
 
     MouseArea {
         id: mouseArea
-        anchors.fill: parent
+        anchors.fill: overlay
         hoverEnabled: true
-        property var last_pos: null;
 
         onPressed: {
-            var curr = Qt.point(mouse.x, mouse.y)
-            canvas.mouse_pos.push(curr)
-            last_pos = Qt.point(curr.x, curr.y)
+            var wf = media.sourceWidth/width
+            var hf = media.sourceHeight/height
+            var curr = Qt.point(mouse.x*wf, mouse.y*hf)
+
+            canvas.mousePressed(curr)
         }
 
         onReleased: {
-            canvas.mouse_pos = []
+            var wf = media.sourceWidth/width
+            var hf = media.sourceHeight/height
+            var curr = Qt.point(mouse.x*wf, mouse.y*hf)
+
+            canvas.mouseReleased(curr)
         }
 
         onPositionChanged: {
-            var s = root.size*root.spacing;
-
             if(mouse.buttons) {
-                var curr = Qt.point(mouse.x, mouse.y)
-                if(last_pos == null) {
-                    canvas.mouse_pos.push(curr)
-                    last_pos = Qt.point(curr.x, curr.y)
-                } else {
-                    var last = Qt.point(last_pos.x, last_pos.y)
-                    var v = Qt.point(curr.x-last.x, curr.y-last.y)
-                    var m = Math.pow(v.x*v.x + v.y*v.y, 0.5)
-                    var r = m/s
-                    if(r < 1) {
-                        return;
-                    }
-                    var p;
-                    for(var i = 1; i <= r; i++) {
-                        var f = i*s/m
-                        p = Qt.point(last.x+v.x*f, last.y+v.y*f)
-                        canvas.mouse_pos.push(p)
-                    }
-                    last_pos = Qt.point(p.x, p.y)
-                }
+                var wf = media.sourceWidth/width
+                var hf = media.sourceHeight/height
+                var curr = Qt.point(mouse.x*wf, mouse.y*hf)
+
+                canvas.mouseDragged(curr)
             }
-        }
-    }
-    Keys.onPressed: {
-        event.accepted = true
-        switch(event.key) {
-        case Qt.Key_R:
-            canvas.do_reset = true
-            break;
-        default:
-            event.accepted = false
         }
     }
 }
