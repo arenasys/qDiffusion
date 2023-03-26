acf1d7749ee80b16e604db49f4e95c3cd380f0f6
arena <arenatemp@mailfence.com>
2023-03-26 21:49:24 +0800

    add start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,6 @@
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
+@echo off
+
 if not exist .\venv\ (
 	mkdir venv
 	python -m venv .\venv\
@@ -8,4 +11,6 @@ if not exist .\venv\ (
 	start /wait /b "" .\venv\Scripts\pip3.exe install opencv-python-headless
 	start /wait /b "" .\venv\Scripts\pip3.exe install xformers
 )
+
 .\venv\Scripts\python.exe .\main.py
+exit
