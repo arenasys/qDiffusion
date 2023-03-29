eedf57e4e037355c349e33ca23317745851c6c61
arena <arenatemp@mailfence.com>
2023-03-29 17:10:18 +0800

    script tweak

--- start_win.bat
+++ start_win.bat
@@ -1,15 +1,21 @@
 @echo off
 
 if not exist .\venv\ (
+	echo "INITIALIZING..."
 	mkdir venv
 	python -m venv .\venv\
-	start /wait /b "" .\venv\Scripts\pip3.exe install torch torchvision --index-url https://download.pytorch.org/whl/cu117
-	start /wait /b "" .\venv\Scripts\pip3.exe install -r .\requirements.txt
-	start /wait /b "" .\venv\Scripts\pip3.exe install basicsr
-	start /wait /b "" .\venv\Scripts\pip3.exe uninstall --yes opencv-python
-	start /wait /b "" .\venv\Scripts\pip3.exe install opencv-python-headless
-	start /wait /b "" .\venv\Scripts\pip3.exe install xformers
+	call .\venv\Scripts\activate.bat
+	start /wait /b pip3 install --upgrade pip
+	start /wait /b pip3 install wheel
+	start /wait /b pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu117
+	start /wait /b pip3 install -r .\requirements.txt
+	start /wait /b pip3 install xformers
+	start /wait /b pip3 install basicsr
+	start /wait /b pip3 uninstall --yes opencv-python
+	start /wait /b pip3 install opencv-python-headless
+) else (
+	call .\venv\Scripts\activate.bat
 )
 
-.\venv\Scripts\python.exe .\main.py
+start pythonw .\main.py
 exit
