3d1f988e4390d11fd24c4debabdd444c34fdd926
arena <arenatemp@mailfence.com>
2023-04-03 03:27:14 +0800

    launchers only use launch
    Former-commit-id: e65620b41a7868ee31ad16fce56c8014a05f369d

--- start_linux.sh
+++ start_linux.sh
@@ -1,11 +1,1 @@
-if [ ! -d "venv" ] 
-then
-	echo INITIALIZING...
-	python -m venv venv
-	source venv/bin/activate
-	pip install PyQt5==5.15.7
-else
-    source venv/bin/activate
-fi
-
 python source/launch.py
--- start_win.bat
+++ start_win.bat
@@ -1,13 +1,3 @@
 @echo off
-
-if not exist .\venv\ (
-	echo INITIALIZING...
-	python -m venv venv
-	call .\venv\Scripts\activate.bat
-	start /wait /b pip install "PyQt5==5.15.7"
-) else (
-	call .\venv\Scripts\activate.bat
-)
-
 start python source\launch.py
 exit
