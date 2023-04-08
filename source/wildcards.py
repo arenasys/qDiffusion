1a75bcee64d74ca4e2047427c81422ea3c0fc729
arena <arenatemp@mailfence.com>
2023-04-08 13:49:35 +0800

    wildcard support
    Former-commit-id: 5a655a6db0061a56e2708d0412f5796730a3ad45

---
+++ source/wildcards.py
@@ -0,0 +1,37 @@
+from PyQt5.QtCore import pyqtSlot, pyqtProperty, pyqtSignal, QObject, QThread, Qt
+import os, re, glob, time
+
+import filesystem
+
+class Wildcards(QObject):
+    updated = pyqtSignal()
+    def __init__(self, gui):
+        super().__init__(gui)
+        self.gui = gui
+        self._folders = []
+        self._wildcards = {}
+        self.watcher = filesystem.Watcher.instance
+        self.watcher.folder_changed.connect(self.onFolderChanged)
+        self.reload()
+
+    @pyqtSlot()
+    def reload(self):
+        self._wildcards = {}
+        root = self.gui.config.get("model_directory")
+        folders = [os.path.join(root,f) for f in os.listdir(root) if os.path.isdir(os.path.join(root,f))]
+        self._folders = [f for f in folders if re.search('wildcard(s)?', f, re.IGNORECASE)]    
+        for folder in self._folders:
+            self.watcher.watchFolder(folder)
+    
+    def loadFile(self, file):
+        with open(file, 'r') as f:
+            lines = [l.strip() for l in f.readlines() if l.strip()]
+            if lines:
+                name = file.rsplit(os.path.sep,1)[-1].rsplit('.',1)[0]
+                self._wildcards[name] = lines
+        self.updated.emit()
+
+    @pyqtSlot(str, str, int)
+    def onFolderChanged(self, folder, file, idx):
+        if folder in self._folders and file.endswith(".txt"):
+            self.loadFile(file)
