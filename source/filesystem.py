cbb0e210dae0032711a035cd1cfce51110eadd81
arena <arenatemp@mailfence.com>
2023-04-21 02:16:24 +0800

    watcher load order fix
    Former-commit-id: 8f360f3e8e88b8b18ee930cf3c27479f09706ca9

--- source/filesystem.py
+++ source/filesystem.py
@@ -26,7 +26,7 @@ class WatcherRunnable(QRunnable):
     def run(self):
         try:
             files = glob.glob(os.path.join(self.folder, "*.*"))
-            files = sorted(files, key = os.path.getmtime)
+            files = sorted(files, key = os.path.getmtime, reverse=True)
             for i, file in enumerate(files):
                 if self.signals.stopping:
                     return
