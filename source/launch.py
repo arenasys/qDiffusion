e58ad0533e351ae9f86d499a87fa8d5b80711053
arena <arenatemp@mailfence.com>
2023-05-14 23:13:58 +0800

    installer unicode fix
    Former-commit-id: c5d6b90fbc9173d96b8c32310424369a8baef9a3

--- source/launch.py
+++ source/launch.py
@@ -2,17 +2,15 @@ import sys
 import subprocess
 import os
 import platform
-import time
+import shutil
+import importlib.util
+
+VENV_DIR = "venv"
 IS_WIN = platform.system() == 'Windows'
-QT_VER = "PyQt5==5.15.7"
-VENV_DIR = os.path.join(os.getcwd(), "venv")
-
-INSIDE_VENV = VENV_DIR in sys.executable and VENV_DIR in os.environ["PATH"]
-MISSING_VENV = not os.path.exists("venv")
-MISSING_QT = False
-
 PYTHON_RUN = sys.executable
 
+QT_VER = "PyQt5==5.15.7"
+MISSING_QT = False
 try:
     from PyQt5.QtCore import Qt
 except Exception:
@@ -29,33 +27,61 @@ def get_env():
 
 def restart():
     if IS_WIN:
-        subprocess.Popen(("venv\\Scripts\\pythonw source\\launch.py").split(' '), env=get_env(), creationflags=0x00000008|0x00000200)
+        subprocess.Popen([os.path.join(VENV_DIR, "Scripts\\pythonw"), "source\\launch.py"], env=get_env(), creationflags=0x00000008|0x00000200)
     else:
-        subprocess.Popen(("venv/bin/python source/launch.py").split(' '), env=get_env())
+        print(os.getcwd())
+        subprocess.Popen([os.path.join(VENV_DIR, "bin/python"), "source/launch.py"], env=get_env())
     exit()
 
 def install_venv():
     print("CREATING VENV...")
-    subprocess.run(f"{PYTHON_RUN} -m venv venv".split(' '))
+    subprocess.run((f"{PYTHON_RUN} -m venv " + VENV_DIR).split(' '))
 
 def install_qt():
     print("INSTALLNG PyQt...")
     if IS_WIN:
-        subprocess.run(("venv\\Scripts\\pip install "+QT_VER).split(' '), env=get_env())
+        subprocess.run([os.path.join(VENV_DIR, "Scripts\\pip"), "install", QT_VER], env=get_env())
     else:
-        subprocess.run(("venv/bin/pip install "+QT_VER).split(' '), env=get_env())
+        subprocess.run([os.path.join(VENV_DIR, "bin/pip"), "install", QT_VER], env=get_env())
 
 if __name__ == "__main__":
     if sys.version_info[0] < 3 or sys.version_info[1] < 8:
-        raise Exception(f"Python 3.8 or greater required. Have Python {sys.version_info[0]}.{sys.version_info[1]}.")
+        print(f"Python 3.8 or greater is required. Have Python {sys.version_info[0]}.{sys.version_info[1]}.")
+        exit()
+    if not importlib.util.find_spec("pip"):
+        print("PIP module is required.")
+        exit()
+    if not importlib.util.find_spec("venv"):
+        print("VENV module is required.")
+        exit()
+    if not shutil.which("git"):
+        print("Git is required.")
+        exit()
+    
+    if len(sys.argv) > 1:
+        VENV_DIR = sys.argv[1]
+    VENV_DIR = os.path.abspath(VENV_DIR)
 
-    if not INSIDE_VENV:
-        if MISSING_VENV:
+    invalid = ''.join([c for c in VENV_DIR if ord(c) > 127])
+    if invalid:
+        print(f"PATH INVALID ({VENV_DIR}) CONTAINS UNICODE ({invalid})")
+        if IS_WIN:
+            VENV_DIR = os.getcwd()[0]+":\\qDiffusion"
+            print(f"USING {VENV_DIR} INSTEAD")
+        else:
+            print("FAILED")
+            exit()
+
+    inside_venv = VENV_DIR in sys.executable and VENV_DIR in os.environ["PATH"]
+    missing_venv = not os.path.exists(VENV_DIR)
+
+    if not inside_venv:
+        if missing_venv:
             install_venv()
             install_qt()
             print("DONE.")
         restart()
-    elif INSIDE_VENV and MISSING_QT:
+    elif inside_venv and MISSING_QT:
         install_qt()
 
     import main
