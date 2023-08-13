31c400d55325a40427b04635f659bc14b43ad4c1
arena <arenatemp@mailfence.com>
2023-08-13 12:31:53 +0800

    bump python versions
    Former-commit-id: 42b4016b1582f9da757f79175b08448d9dd98222

--- start_linux.sh
+++ start_linux.sh
@@ -1,9 +1,10 @@
 if [ ! -d "./python" ] 
 then
     echo "DOWNLOADING PYTHON..."
-    curl -L --progress-bar "https://github.com/indygreg/python-build-standalone/releases/download/20230507/cpython-3.10.11+20230507-x86_64_v3-unknown-linux-gnu-install_only.tar.gz" -o "python-3.10.11.tar.gz"
+    curl -L --progress-bar "https://github.com/indygreg/python-build-standalone/releases/download/20230726/cpython-3.10.12+20230726-x86_64_v3-unknown-linux-gnu-install_only.tar.gz" -o "python.tar.gz"
+    
     echo "EXTRACTING PYTHON..."
-    tar -xf "python-3.10.11.tar.gz"
-    rm "python-3.10.11.tar.gz"
+    tar -xf "python.tar.gz"
+    rm "python.tar.gz"
 fi
 ./python/bin/python3 source/launch.py
--- start_win.bat
+++ start_win.bat
@@ -1,10 +1,19 @@
 @echo off
+
+for /f "tokens=4-7 delims=[.] " %%i in ('ver') do (if %%i==Version (set v=%%j.%%k) else (set v=%%i.%%j))
+echo "%v"
+IF "%v%" == "6.1" (
+    set python_version = "20230726/cpython-3.8.16+20230726-x86_64-pc-windows-msvc-shared-install_only.tar.gz"
+) ELSE (
+    set python_version = "20230726/cpython-3.10.12+20230726-x86_64-pc-windows-msvc-shared-install_only.tar.gz"
+)
+
 IF NOT EXIST "python" (
     echo DOWNLOADING PYTHON...
-    bitsadmin.exe /transfer "DOWNLOADING PYTHON..." "https://github.com/indygreg/python-build-standalone/releases/download/20230507/cpython-3.10.11+20230507-x86_64-pc-windows-msvc-shared-install_only.tar.gz" "%CD%/python-3.10.11.tar.gz"
+    bitsadmin.exe /transfer "DOWNLOADING PYTHON..." "https://github.com/indygreg/python-build-standalone/releases/download/%python_version%" "%CD%/python.tar.gz"
     echo EXTRACTING PYTHON...
-    tar -xf "python-3.10.11.tar.gz"
-    del /Q "python-3.10.11.tar.gz"
+    tar -xf "python.tar.gz"
+    del /Q "python.tar.gz"
 )
 start .\python\python.exe source\launch.py
 exit
