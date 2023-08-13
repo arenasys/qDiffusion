721b0a6b33fd48da7b39b932e2f6a0e58ab762df
arena <arenatemp@mailfence.com>
2023-08-13 13:03:11 +0800

    start_win.bat fix
    Former-commit-id: bd9ab1442c0d3c4f538ce2b67be610a088302e1b

--- start_win.bat
+++ start_win.bat
@@ -1,19 +1,19 @@
 @echo off
-
 for /f "tokens=4-7 delims=[.] " %%i in ('ver') do (if %%i==Version (set v=%%j.%%k) else (set v=%%i.%%j))
-echo "%v"
-IF "%v%" == "6.1" (
-    set python_version = "20230726/cpython-3.8.16+20230726-x86_64-pc-windows-msvc-shared-install_only.tar.gz"
-) ELSE (
-    set python_version = "20230726/cpython-3.10.12+20230726-x86_64-pc-windows-msvc-shared-install_only.tar.gz"
-)
 
 IF NOT EXIST "python" (
     echo DOWNLOADING PYTHON...
-    bitsadmin.exe /transfer "DOWNLOADING PYTHON..." "https://github.com/indygreg/python-build-standalone/releases/download/%python_version%" "%CD%/python.tar.gz"
+
+    IF "%v%" == "10.0" (
+        bitsadmin.exe /transfer "DOWNLOADING PYTHON 3.10..." "https://github.com/indygreg/python-build-standalone/releases/download/20230726/cpython-3.10.12+20230726-x86_64-pc-windows-msvc-shared-install_only.tar.gz" "%CD%/python.tar.gz"
+    ) else (
+        bitsadmin.exe /transfer "DOWNLOADING PYTHON 3.8..." "https://github.com/indygreg/python-build-standalone/releases/download/20230726/cpython-3.8.16+20230726-x86_64-pc-windows-msvc-shared-install_only.tar.gz" "%CD%/python.tar.gz"
+    )
+
     echo EXTRACTING PYTHON...
     tar -xf "python.tar.gz"
     del /Q "python.tar.gz"
 )
+
 start .\python\python.exe source\launch.py
 exit
