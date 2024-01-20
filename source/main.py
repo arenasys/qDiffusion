d147805d399d979319f0a8f782924a74e42f8d1c
arena <arenatemp@mailfence.com>
2024-01-21 00:04:23 +0800

    training reqs

--- source/main.py
+++ source/main.py
@@ -1,6 +1,7 @@
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning) 
 warnings.filterwarnings("ignore", category=DeprecationWarning) 
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 import sys
 import signal
