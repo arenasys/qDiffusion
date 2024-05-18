e6d999cc2bea0100d626586b7b827a11bcd43637
arenasys <arenatemp@mailfence.com>
2024-05-18 10:17:40 +0800

    start.sh old cpu support

--- source/start.sh
+++ source/start.sh
@@ -22,8 +22,19 @@ cd ..
 
 if [ ! -d "./python" ] 
 then
-    echo "DOWNLOADING PYTHON..."
-    curl -L --progress-bar "https://github.com/indygreg/python-build-standalone/releases/download/20230726/cpython-3.10.12+20230726-x86_64_v3-unknown-linux-gnu-install_only.tar.gz" -o "python.tar.gz"
+    flags=$(grep flags /proc/cpuinfo)
+    arch="x86_64"
+    if [[ $flags == *"sse4"* ]]; then
+        arch="x86_64_v2"
+    fi
+    if [[ $flags == *"avx2"* ]]; then
+        arch="x86_64_v3"
+    fi
+    if [[ $flags == *"avx512"* ]]; then
+        arch="x86_64_v4"
+    fi
+    echo "DOWNLOADING PYTHON ($arch)..."
+    curl -L --progress-bar "https://github.com/indygreg/python-build-standalone/releases/download/20230726/cpython-3.10.12+20230726-$arch-unknown-linux-gnu-install_only.tar.gz" -o "python.tar.gz"
     
     echo "EXTRACTING PYTHON..."
     tar -xf "python.tar.gz"
