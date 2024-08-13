b36683e50cf1e7f52091e3e12450bc3cdc234687
arenasys <arenatemp@mailfence.com>
2024-08-13 17:33:44 +0800

    mac fix

--- source/start-mac.sh
+++ source/start-mac.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "./python" ] 
 then
-    echo "DOWNLOADING PYTHON ($arch)..."
+    echo "DOWNLOADING PYTHON..."
     curl -L --progress-bar "https://github.com/indygreg/python-build-standalone/releases/download/20240726/cpython-3.10.14+20240726-x86_64-apple-darwin-install_only.tar.gz" -o "python.tar.gz"
     echo "EXTRACTING PYTHON..."
     tar -xf "python.tar.gz"
